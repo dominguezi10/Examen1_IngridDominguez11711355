306b051cb15a74f1a5df8bd43316528eb09737f0
dominguezi10 <dominguezi10@unitec.edu>
2017-11-10 13:28:27 -0600

    inicio del proyecto primera clase

---
+++ Examen1_Ingrid_111711355/src/Persona.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1234
+ */
+public class Persona {
+    private String nombre;
+    private String edad;
+    private int id;
+    private String sexo;
+    private String estado_civil;
+    private ArrayList<String> lista = new ArrayList();
+    
+    
+}
